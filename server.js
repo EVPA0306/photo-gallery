32f0551bdf8e03d5e9b2434d7abade1ce71204f1
Eugene Pavlenko <Eugene Pavlenko@RaymondJames.com>
2021-01-25 21:03:10 -0700

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // server.js
+const path = require('path');
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
@@ -7,3 +8,8 @@ app.use(express.static(__dirname + '/www'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/www/index.html'));
+  });
